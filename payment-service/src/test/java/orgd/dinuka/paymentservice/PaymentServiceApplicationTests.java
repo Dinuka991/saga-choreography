19a1dfaba97a0ff12350b19dbeb4faa124155090
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-07-07 07:05:41 +0800

    init

---
+++ payment-service/src/test/java/orgd/dinuka/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package orgd.dinuka.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $orgd.dinuka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
